b5d87192072e2b2dadf453c0df4bcecacb931678
Lidia <lidia.wer@mail.ru>
2022-09-10 20:13:05 +0300

    задачи 41 и43

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Console.Write("Введите числа через пробел: ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = arr.Count(i => i > 0);
+// Console.WriteLine($"Кол-во чисел больше 0: {count}");
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Console.WriteLine("введите b1");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("введите k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("введите b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("введите k2");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (b1 - b2)/(k2 - k1);
+// double y = k1 * x + b1;
+
+// Console.WriteLine($"пересечение двух прямых в точке с координатами X: {x}, Y: {y}");
